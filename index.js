833ae1359f18f587930b7f69fac5cfaaa8591a8d
Rique-Yazzie <rty7@nau.edu>
2026-09-14 20:18:48 -0700

    added edit and delete functionality to website

--- index.js
+++ index.js
@@ -1,13 +1,78 @@
+// to add changes to github use these:
+    // git add .
+    // git commit -m "your message"
+    // git push
 import express from "express";
 
 const app = express();
 
+// array for temporarily storing posts, will dissapear when we restart the server
+let posts = [];
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = 3000;
+
+app.set("view engine", "ejs");
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    // pass posts array to EJS to display blog posts on the home page
+    res.render("index", { posts: posts });
+});
+
+// route used for editing a specific post
+app.get("/posts/:id/edit", (req, res) => {
+
+    const postId = Number(req.params.id);
+
+    const post = posts.find(post => post.id === postId);
+
+    // open file edit.ejs and give it the post we found
+    res.render("edit", { post: post });
+
+});
+
+// route used to save changes to a specific post
+app.post("/posts/:id/edit", (req, res) => {
+
+    const postId = Number(req.params.id);
+
+    const post = posts.find(post => post.id === postId);
+
+    post.creator = req.body.creator;
+    post.title = req.body.title;
+    post.content = req.body.blog;
+
+    res.redirect("/");
+});
+
+// route used to delete a specific post
+app.post("/posts/:id/delete", (req, res) => {
+
+    const postId = Number(req.params.id);
+
+    posts = posts.filter(post => post.id !== postId);
+
+    res.redirect("/");
+});
+
+app.post("/posts", (req, res) => {
+    // create a new post object with the data from the request body
+    const newPost = {
+        id: Date.now(),
+        // get name of creator, title, and content from the request body
+        creator: req.body.creator,
+        title: req.body.title,
+        content: req.body.blog,
+        date: new Date()
+    };
+
+    // push post into server side array
+    posts.push(newPost);
+
+    // send user back to the home page after they submit a post
+    res.redirect("/");
 });
