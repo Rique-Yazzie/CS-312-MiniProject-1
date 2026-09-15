2d82b6b08e2bd1399b94ae2d14f0cfcea6ea4f4f
Rique-Yazzie <rty7@nau.edu>
2026-09-14 21:51:16 -0700

    final comments, cleaning, etc

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const app = express();
 
 // array for temporarily storing posts, will dissapear when we restart the server
 let posts = [];
+
+// use browser to access files in public css folder
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 const PORT = 3000;
 
@@ -21,6 +24,25 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
     // pass posts array to EJS to display blog posts on the home page
     res.render("index", { posts: posts });
+});
+
+// POST route used to create a new post
+app.post("/posts", (req, res) => {
+    // create a new post object with the data from the request body
+    const newPost = {
+        id: Date.now(),
+        // get name of creator, title, and content from the request body
+        creator: req.body.creator,
+        title: req.body.title,
+        content: req.body.blog,
+        date: new Date()
+    };
+
+    // push post into server side array
+    posts.push(newPost);
+
+    // send user back to the home page after they submit a post
+    res.redirect("/");
 });
 
 // route used for editing a specific post
@@ -42,10 +64,12 @@ app.post("/posts/:id/edit", (req, res) => {
 
     const post = posts.find(post => post.id === postId);
 
+    // takes new info and updates original post object with new info
     post.creator = req.body.creator;
     post.title = req.body.title;
     post.content = req.body.blog;
 
+    // redirect back to the home page after saving changes
     res.redirect("/");
 });
 
@@ -56,23 +80,6 @@ app.post("/posts/:id/delete", (req, res) => {
 
     posts = posts.filter(post => post.id !== postId);
 
-    res.redirect("/");
-});
-
-app.post("/posts", (req, res) => {
-    // create a new post object with the data from the request body
-    const newPost = {
-        id: Date.now(),
-        // get name of creator, title, and content from the request body
-        creator: req.body.creator,
-        title: req.body.title,
-        content: req.body.blog,
-        date: new Date()
-    };
-
-    // push post into server side array
-    posts.push(newPost);
-
-    // send user back to the home page after they submit a post
+    // redirect back to the home page after deleting the post
     res.redirect("/");
 });
