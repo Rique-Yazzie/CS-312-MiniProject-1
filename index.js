7bc9527c142616665181ca129c56a36d56f960da
Rique-Yazzie <rty7@nau.edu>
2026-09-14 19:21:24 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
